73654c540832bdb152a0995f3d372e435feb5766
Ivan Krišto <ikr@google.com>
2022-09-23 15:15:03 +0200

    hack around glibc issue

--- .github/bin/install-verible.sh
+++ .github/bin/install-verible.sh
@@ -7,19 +7,17 @@ set -e
 
 echo "Fetching Verible"
 
+# Fetch the latest Verible release for Ubuntu.
+# But, don't take the latest Ubuntu version as Github Ubuntu images aren't
+# updated as fast as Verible releases.
 LATEST_VERIBLE_RELEASE=$(curl -s \
   https://api.github.com/repos/chipsalliance/verible/releases/latest \
   | grep -o "https:.*Ubuntu.*tar.gz" \
   | sort -r \
-  | head -n1)
+  | head -n2 \
+  | tail -n1)
 
 mkdir /tmp/verible-bin
 cd /tmp/verible-bin
 wget --no-verbose -O verible.tar.gz "${LATEST_VERIBLE_RELEASE}"
 tar -xzf verible.tar.gz --strip-components=1
-
-# Update glibc for Verible
-apt-cache policy libc6
-sudo apt update
-sudo apt install libc6
-apt-cache policy libc6
